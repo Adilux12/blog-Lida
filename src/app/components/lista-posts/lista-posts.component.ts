26cb4b6ac47d030c32580513990eadd5e4dc75f4
LidaXu <xulida12@gmail.com>
2023-03-15 23:08:24 +0100

    crea funciones de componente lista-posts y prueba si funciona

--- src/app/components/lista-posts/lista-posts.component.ts
+++ src/app/components/lista-posts/lista-posts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Post } from 'src/app/interfaces/post.interface';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-lista-posts',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-posts.component.css']
 })
 export class ListaPostsComponent {
+  posts: Post[];
+  constructor(private postService: PostService) {
+    this.posts = [];
+  }
 
+  ngOnInit() {
+    this.posts = this.postService.getAll();
+  }
+
+  onChange($event: any) {
+    console.log($event.target.value)
+    if ($event.target.value === 'todos') {
+      this.posts = this.postService.getAll();
+    }
+    this.posts = this.postService.getByCategory($event.target.value)
+  }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -7,7 +7,24 @@ import { Post } from '../interfaces/post.interface';
 export class PostService {
   arrPosts: Post[];
   constructor() {
-    this.arrPosts = []
+    this.arrPosts = [
+      {
+        titulo: 'Hamburguesa Delicioso',
+        texto: 'Receta hamburguesa',
+        autor: 'Lida',
+        imagen: 'https://img.freepik.com/fotos-premium/hamburguesa-deliciosa-casera-fresca-bollo-espinacas-aislado-blanco_147620-1711.jpg',
+        fecha: new Date(22 / 3 / 2022),
+        categoria: "Comida"
+      },
+      {
+        titulo: 'Mi primera web',
+        texto: 'Enseña paso a paso la creación de web',
+        autor: 'Lida',
+        imagen: 'https://www.uplers.com/wp-content/uploads/2022/05/AngularJs-Frameworks-891x505.jpg',
+        fecha: new Date(22 / 3 / 2022),
+        categoria: "Tutorial"
+      }
+    ]
   }
 
 
