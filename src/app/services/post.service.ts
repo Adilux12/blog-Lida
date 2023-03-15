7c10b2aefbabdfd6da8e05ac4acc75d810d0b680
LidaXu <xulida12@gmail.com>
2023-03-15 22:00:12 +0100

    crea funciones para post.service.ts de archivo services

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../interfaces/post.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  arrPosts: Post[];
+  constructor() {
+    this.arrPosts = []
+  }
 
-  constructor() { }
+
+  getAll() {
+    return this.arrPosts;
+  }
+
+  create(pPost: any) {
+    this.arrPosts.push(pPost);
+  }
+
+  getByCategory(category: string) {
+    return this.arrPosts.filter(post => post.categoria === category)
+  }
+
+
 }
